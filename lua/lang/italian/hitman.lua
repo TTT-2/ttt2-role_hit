06bb97bbed6d37a88f04f5713d4f5494c7efd980
Alessandro Levante <62677560+PinoMartirio@users.noreply.github.com>
2020-04-01 19:49:42 +0200

    Added Italian translation
    Added full Italian translation for the role Hitman.

---
+++ lua/lang/italian/hitman.lua
@@ -0,0 +1,21 @@
+L = LANG.GetLanguageTableReference("italian")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HITMAN.name] = "Sicario"
+L["info_popup_" .. HITMAN.name] = [[Sei un Sicario!
+Prova a prendere dei crediti!]]
+L["body_found_" .. HITMAN.abbr] = "Era un Sicario!"
+L["search_role_" .. HITMAN.abbr] = "Questa persona era un Sicario!"
+L["target_" .. HITMAN.name] = "Sicario"
+L["ttt2_desc_" .. HITMAN.name] = [[Il Sicario è un Traditore che lavora insieme agli altri traditore con lo scopo di uccidere tutti tranne i traditori.
+Il Sicario può raccogliere crediti solo se uccide il suo bersaglio.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+if amount == 1 then
+	L["ttt2_hitman_target_killed_credits"] = "Hai ricevuto {amount} credito per aver ucciso il tuo bersaglio."
+else
+	L["ttt2_hitman_target_killed_credits"] = "Hai ricevuto {amount} crediti per aver ucciso il tuo bersaglio."
+end
+L["ttt2_hitman_target_killed"] = "Hai ucciso il tuo bersaglio!"
+L["ttt2_hitman_chat_reveal"] = "'{playername}' è un Sicario!"
+L["ttt2_hitman_target_died"] = "Il tuo bersaglio è morto..."
