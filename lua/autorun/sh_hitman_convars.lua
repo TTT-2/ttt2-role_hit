de0763ed94bcef4e509a1eb29112bc9d246af591
Sebi <Polarfuchs123@gmx.de>
2020-06-11 12:05:15 +0200

    Option to toggle wether or not a spy should be able to be a hitmans target

--- lua/autorun/sh_hitman_convars.lua
+++ lua/autorun/sh_hitman_convars.lua
@@ -1,5 +1,6 @@
 CreateConVar("ttt2_hitman_target_credit_bonus", 1, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
 CreateConVar("ttt2_hitman_target_chatreveal", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+CreateConVar("ttt2_hitman_target_can_be_spy", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
 
 hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_hitman_convars", function(tbl)
 	tbl[ROLE_HITMAN] = tbl[ROLE_HITMAN] or {}
@@ -17,5 +18,11 @@ hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_hitman_convars", function(tbl)
 		cvar = "ttt2_hitman_target_chatreveal",
 		checkbox = true,
 		desc = "ttt2_hitman_target_chatreveal (def. 0)"
+	})
+	
+	table.insert(tbl[ROLE_HITMAN], {
+		cvar = "ttt2_hitman_target_can_be_spy",
+		checkbox = true,
+		desc = "ttt2_hitman_target_can_be_spy (def. 0)"
 	})
 end)
