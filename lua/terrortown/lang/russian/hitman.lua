516101e550667285a4bc41c6f1461b5b8d6ab2a3
Blueberryy <igory.ygr200@gmail.com>
2020-09-27 21:46:59 +0500

    Create hitman.lua

---
+++ lua/terrortown/lang/russian/hitman.lua
@@ -0,0 +1,18 @@
+local L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HITMAN.name] = "Хитмэн"
+L["info_popup_" .. HITMAN.name] = [[Вы хитмэн!
+Попытайтесь получить кредиты!]]
+L["body_found_" .. HITMAN.abbr] = "Он был хитмэном!"
+L["search_role_" .. HITMAN.abbr] = "Этот человек был хитмэном!"
+L["target_" .. HITMAN.name] = "Хитмэн"
+L["ttt2_desc_" .. HITMAN.name] = [[Хитмэн - предатель, работающий вместе с другими предателями с целью убить всех других игроков, не являющихся предателями.
+
+Хитмэн может просто получить несколько кредитов, если убьёт свою цель.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_hitman_target_killed_credits"] = "Вы получили {amount} кредит(а/ов) за устранение цели."
+L["ttt2_hitman_target_killed"] = "Вы убили свою цель!"
+L["ttt2_hitman_chat_reveal"] = "'{playername}' хитмэн!"
+L["ttt2_hitman_target_died"] = "Ваша цель умерла..."
