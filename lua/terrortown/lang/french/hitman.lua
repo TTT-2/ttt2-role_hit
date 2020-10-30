dd40c8efecc1521bec2fec162f2e018bb1f8e45b
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:45:52 +0100

    Create hitman.lua (#16)
    french translation :Hitman

---
+++ lua/terrortown/lang/french/hitman.lua
@@ -0,0 +1,17 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HITMAN.name] = "Tueur à Gages"
+L["info_popup_" .. HITMAN.name] = [[Vous êtes un Tueur à Gages!
+Essayez d'obtenir des crédits!]]
+L["body_found_" .. HITMAN.abbr] = "C'était un Tueur à Gages!"
+L["search_role_" .. HITMAN.abbr] = "Cette personne était un Tueur à Gages!"
+L["target_" .. HITMAN.name] = "Tueur à Gages"
+L["ttt2_desc_" .. HITMAN.name] = [[Le Tueur à Gages est un traître qui travaille avec les autres traîtres dans le but de tuer tous les autres joueurs qui ne sont pas dans son équipe.
+Le Tueur à Gages est juste capable de collecter quelques crédits s'il tue sa cible.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+L["ttt2_hitman_target_killed_credits"] = "Vous avez reçu {amount} crédit(s) pour avoir éliminé votre cible."
+L["ttt2_hitman_target_killed"] = "Vous avez tué votre cible!"
+L["ttt2_hitman_chat_reveal"] = "'{playername}' est  un Tueur à Gages!"
+L["ttt2_hitman_target_died"] = "Votre cible est morte..."
